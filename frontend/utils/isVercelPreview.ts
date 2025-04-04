0218a22419e8b57bb407f432909ad40967ff1d7b
jthcast <jthcast@gmail.com>
2025-04-04 11:26:47 +0900

    fix: production check

--- frontend/utils/isVercelPreview.ts
+++ frontend/utils/isVercelPreview.ts
@@ -1,5 +1,10 @@
 export const isVercelPreview = () => {
+  const isProduction = process.env.NEXT_PUBLIC_VERCEL_ENV === 'production'
   const vercelUrl = process.env.NEXT_PUBLIC_VERCEL_URL
+
+  if (isProduction) {
+    return false
+  }
 
   if (typeof window !== 'undefined') {
     return vercelUrl ? window.location.hostname.includes(vercelUrl) : false
